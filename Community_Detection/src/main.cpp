f23ead3ee485a9b3207134dfdb4c87248a4b0b12
gaurav38 <grv_srf@yahoo.co.in>
2013-10-05 15:12:32 -0400

    Commiting test file

---
+++ Community_Detection/src/main.cpp
@@ -0,0 +1,21 @@
+#include "Node.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Node *node = new Node(1);
+    std::cout<<"Label="<<node->Getlabel();
+    for(int i=2; i<=10; i++)
+        node->AddNeighbor(i);
+    Neighbors neighbors = node->Getneighbors();
+    Neighbors::iterator it;
+    for(it=neighbors.begin(); it<neighbors.end(); it++)
+        std::cout<<"\nNeighbors="<<*it;
+
+    node->AddCommunity(3);
+    node->AddCommunity(3);
+    node->AddCommunity(2);
+    std::cout<<"\nCount for Community 3="<<node->GetCommunityCount(3);
+    std::cout<<"\nTotal Count="<<node->Getcount();
+}
