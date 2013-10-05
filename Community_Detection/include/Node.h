b165e580ddb456ee65432028d4c2ac708789c9a1
gaurav38 <grv_srf@yahoo.co.in>
2013-10-05 15:11:39 -0400

    Revision-1 for Node.h and Node.cpp

--- Community_Detection/include/Node.h
+++ Community_Detection/include/Node.h
@@ -1,44 +1,58 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+typedef unsigned int Label;
+typedef std::map<Label, unsigned int> Community_Map;
+typedef std::map<Label, unsigned int> Listener_Map;
+typedef vector<Label> Neighbors;
+typedef std::set<Label> MaxLabel;
 
 class Node
 {
     public:
         /** Default constructor */
         Node();
+        /** Parameterized Constructor */
+        Node(Label node_label)
+        {
+            label = node_label;
+            mymap[label] = 1;
+            community_count++;
+            maxlabel.insert(1);
+        }
         /** Default destructor */
         virtual ~Node();
-        /** Access label
-         * \return The current value of label
-         */
-        Label Getlabel() { return label; }
-        /** Access neighbors
-         * \return The current value of neighbors
-         */
-        vector<unsigned_int> Getneighbors() { return neighbors; }
-        /** Set neighbors
-         * \param val New value to set
-         */
-        void Setneighbors(vector<unsigned_int> val) { neighbors = val; }
-        /** Access mymap
-         * \return The current value of mymap
-         */
-        std::unordered_map<Label,int> Getmymap() { return mymap; }
-        /** Set mymap
-         * \param val New value to set
-         */
-        void Setmymap(std::unordered_map<Label,int> val) { mymap = val; }
+        Label Getlabel();
+        Neighbors Getneighbors();
+        void AddNeighbor(Label);
+
         /** Access count;
          * \return The current value of count;
          */
-        unsigned_int32 Getcount;() { return count;; }
+        unsigned int Getcount() { return community_count; }
+        unsigned int GetListenedCount() { return listen_count; }
+        bool isCommunityPresent(Label);
+        void AddCommunity(Label);
+        unsigned int GetCommunityCount(Label);
+        void AddListenedLabel(Label);
+        bool isLabelListened(Label);
+
     protected:
     private:
         Label label; //!< Member variable "label"
-        vector<unsigned_int> neighbors; //!< Member variable "neighbors"
-        std::unordered_map<Label,int> mymap; //!< Member variable "mymap"
-        unsigned_int32 count;; //!< Member variable "count;"
+        Neighbors neighbors; //!< Member variable "neighbors"
+        Community_Map mymap; //!< Member variable "mymap"
+        Listener_Map mylistener;  //!< Member variable "mylistener"
+        MaxLabel maxlabel;  //!<Member variable "maxlabel"
+        unsigned int community_count; //!< Member variable "count;"
+        unsigned int listen_count;
 };
 
 #endif // NODE_H
--- Community_Detection/src/Node.cpp
+++ Community_Detection/src/Node.cpp
@@ -1,4 +1,6 @@
 #include "Node.h"
+#include <map>
+#include <vector>
 
 Node::Node()
 {
@@ -9,3 +11,83 @@ Node::~Node()
 {
     //dtor
 }
+
+/** Access label
+ * \return The current value of label
+ */
+Label Node::Getlabel()
+{
+    return label;
+}
+
+/** Access neighbors
+ * \return The current value of neighbors
+ */
+Neighbors Node::Getneighbors()
+{
+    return neighbors;
+}
+
+/** Set neighbors
+ * \param val New value to set
+ */
+void Node::AddNeighbor(Label x)
+{
+    neighbors.push_back(x);
+}
+
+/** Find community;
+ * \return true if community found;
+ */
+bool Node::isCommunityPresent(Label x)
+{
+    Community_Map::const_iterator got = mymap.find(x);
+    if(got == mymap.end())
+        return false;
+    else
+        return true;
+}
+
+/** Get Community Frequency;
+ * \return the frequency of a community;
+ */
+unsigned int Node::GetCommunityCount(Label x)
+{
+    if(isCommunityPresent(x))
+    {
+        Community_Map::const_iterator got = mymap.find(x);
+        return got->second;
+    }
+    else
+        return 0;
+}
+
+/** Add a Community;
+ */
+void Node::AddCommunity(Label x)
+{
+    if(isCommunityPresent(x))
+        mymap[x]++;
+    else
+        mymap[x] = 1;
+    community_count++;
+}
+
+void Node::AddListenedLabel(Label x)
+{
+    if(isLabelListened(x))
+        mylistener[x]++;
+    else
+        mylistener[x] = 1;
+    listen_count++;
+}
+
+bool Node::isLabelListened(Label x)
+{
+    Listener_Map::const_iterator got = mylistener.find(x);
+    if(got == mylistener.end())
+        return false;
+    else
+        return true;
+}
+
