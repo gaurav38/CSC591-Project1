f9adcd0020fc3f2c0fae0a622eea4f1995aa4227
freekot <a.melechko@gmail.com>
2013-10-07 12:54:56 -0400

    Added Graph.h - header file for Graph.cpp
    How do we store graph?

---
+++ Community_Detection/include/Graph.h
@@ -0,0 +1,34 @@
+/*
+ * Graph.h - Graph class will maintain the information about a graph (or network in other terms)
+ * by loading from a file which lists: number of nodes, number of edges, which followed by edge list as a pair
+ * (start_vertex_ID end_vertex_ID).
+ *
+ *
+ *
+ */
+
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <fstream>
+
+using namespace std;
+
+class Graph{
+
+public:
+	int NN; //total number of nodes in the graph
+	int NE; //total number of edges in the graph
+
+
+	Graph(string inputFileName);
+	virtual ~Graph();
+
+	void readGraph(string inputFileName);
+
+
+};
+
+
+
+#endif /* GRAPH_H_ */
